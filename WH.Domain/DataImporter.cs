7a86f4a95bff960eda52309ffcc175a9f17c0fa7
Chris Auret <chris.auret@yahoo.co.uk>
2017-03-07 14:00:53 +1100

    DataImport.cs

---
+++ WH.Domain/DataImporter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WH.Domain.Interfaces;
+
+namespace WH.Domain
+{
+    public class DataImporter : IDataImporter
+    {
+        public List<Customer> Import(string[] files)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- WH/Application.cs
+++ WH/Application.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WH.Domain.Interfaces;
 
 namespace WH
 {
     public class Application : IApplication
     {
+        private readonly IDataImporter _dataImporter;
+
+        public Application(IDataImporter dataImporter)
+        {
+            _dataImporter = dataImporter;
+        }
+
         public void Run()
         {
-            throw new NotImplementedException();
+            var customers = _dataImporter.Import(new[] {"Settled.csv", "Unsettled.csv"});
         }
     }
 }
